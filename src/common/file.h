9c08cbf2c91e32505f810e0d2d61e3f533300ea4
vent.fang <vent.fang@gmail.com>
2019-12-23 15:28:50 +0800

    added windows file class

---
+++ src/common/file.h
@@ -0,0 +1,150 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+
+#ifdef WIN32
+#include <Windows.h>
+
+namespace util {
+
+class file
+{
+public:
+  static bool exists(const std::string filepath) {
+    DWORD attr = ::GetFileAttributesA(filepath.c_str());
+    return (INVALID_FILE_ATTRIBUTES != attr) && ((FILE_ATTRIBUTE_DIRECTORY & attr) == 0);
+  }
+
+public:
+  file() = default;
+  ~file() { close(); }
+  file(file&) = delete;
+  file(file&&) = delete;
+  file& operator=(file&) = delete;
+  file& operator=(file&&) = delete;
+
+  bool is_open() {return handle_ != INVALID_HANDLE_VALUE; }
+
+  bool open(const std::string filepath, bool create=true, bool nobuf=false) {
+    if (handle_ != INVALID_HANDLE_VALUE)
+      return true;
+
+    DWORD access_flag = GENERIC_READ | GENERIC_WRITE | FILE_ATTRIBUTE_NORMAL;
+    DWORD create_flag = create ? CREATE_ALWAYS : OPEN_EXISTING;
+    DWORD attr_lag = nobuf ? (FILE_FLAG_NO_BUFFERING | FILE_FLAG_WRITE_THROUGH): FILE_ATTRIBUTE_NORMAL;
+    handle_ = ::CreateFileA(filepath.c_str(), access_flag, 0, NULL, create_flag, attr_lag, NULL);
+    if (INVALID_HANDLE_VALUE == handle_) {
+      error_ = ::GetLastError();
+      return false;
+    }
+
+    this->file_path_ = filepath;
+    return true;
+  }
+
+  void close() {
+    if (handle_ != INVALID_HANDLE_VALUE) {
+      flush();
+      ::CloseHandle(handle_);
+      handle_ = INVALID_HANDLE_VALUE;
+    }
+  }
+
+  bool seek(size_t offset) {
+    if (handle_ == INVALID_HANDLE_VALUE)
+      return false;
+
+    LONG hi = offset >> 32;
+    LONG lo = offset & 0xFFFFFFFF;
+    DWORD ret = ::SetFilePointer(handle_, lo, &hi, FILE_BEGIN);
+    if (ret == INVALID_SET_FILE_POINTER) {
+      error_ = ::GetLastError();
+      return false;
+    }
+    return true;
+  }
+
+  bool read(uint8_t* buff, size_t bytes_to_read) {
+    if (handle_ == INVALID_HANDLE_VALUE)
+      return false;
+
+    DWORD rd_bytes = 0;
+    if (!::ReadFile(handle_, buff, (DWORD)bytes_to_read, &rd_bytes, 0))
+      rd_bytes = 0;
+
+    if (rd_bytes == DWORD(bytes_to_read))
+      return true;
+
+    error_= ::GetLastError();
+    return false;
+  }
+
+  bool write(const uint8_t* data, size_t bytes_to_write) {
+    if (handle_ == INVALID_HANDLE_VALUE)
+      return false;
+
+    BOOL ret = 0;
+    DWORD wr_bytes = 0;
+
+    do {
+      ret = ::WriteFile(handle_, data, (DWORD)bytes_to_write, &wr_bytes, NULL);
+      bytes_to_write -= wr_bytes;
+      data += wr_bytes;
+    } while (ret && bytes_to_write > 0 && wr_bytes > 0);
+
+    if (!ret || bytes_to_write > 0) {
+      error_ = ::GetLastError();
+      return false;
+    }
+
+    return true;
+  }
+
+  bool allocate(size_t bytes) {
+    if (handle_ == INVALID_HANDLE_VALUE)
+      return false;
+
+    size_t file_size = size();
+    if (bytes < file_size)
+      return true;
+
+    LONG hi = bytes >> 32;
+    LONG lo = bytes & 0xFFFFFFFF;
+    DWORD ret = ::SetFilePointer(handle_, lo, &hi, FILE_BEGIN);
+    if (ret == INVALID_SET_FILE_POINTER) {
+      error_ = ::GetLastError();
+      return false;
+    }
+    return TRUE == ::SetEndOfFile(handle_);
+  }
+
+  bool flush() {
+    if (handle_ == INVALID_HANDLE_VALUE)
+      return false;
+
+    if (::FlushFileBuffers(handle_))
+      return true;
+    error_ = ::GetLastError();
+    return false;
+  }
+
+  size_t size() {
+    if (handle_ == INVALID_HANDLE_VALUE)
+      return 0;
+
+    DWORD hi, lo;
+    lo = ::GetFileSize(handle_, &hi);
+    return (((size_t)hi) << 32) + lo;
+  }
+
+  uint64_t last_error() const { return error_; }
+
+private:
+  HANDLE handle_{INVALID_HANDLE_VALUE};
+  std::string file_path_;
+  uint64_t error_{0};
+};
+
+}
+
+#endif
